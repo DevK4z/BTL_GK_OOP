5c19efb3575febb8f31a64be1af7396182cccb4b
Kaz Tran <kaztran198@gmail.com>
2026-05-23 15:20:09 +0700

    Fix basePath for GitHub Pages deployment

--- Website/next.config.ts
+++ Website/next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/TEST" : "",
+  basePath: isProd ? "/BTL_GK_OOP" : "",
   images: {
     unoptimized: true,
   },
